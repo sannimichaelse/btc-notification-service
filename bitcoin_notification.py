8c472a048001fdd46d52704925d7b07f2a829e58
Sanni Michael <sannimichaelse@gmail.com>
2021-06-11 12:41:35 +0100

    update threshold

--- bitcoin_notification.py
+++ bitcoin_notification.py
@@ -14,7 +14,7 @@ if (IFTTT_WEBHOOK_KEY or COIN_MARKETCAP_API_KEY) is None:
     raise Exception("You need to setup environment variables. Check env.example or the README.md for more info")
 
 
-BITCOIN_PRICE_THRESHOLD = 50000 # you can set this to any value
+BITCOIN_PRICE_THRESHOLD = 30000 # you can set this to any value
 BITCOIN_API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 IFTTT_WEBHOOK_URL = 'https://maker.ifttt.com/trigger/{}/with/key/'+IFTTT_WEBHOOK_KEY
 
@@ -31,10 +31,12 @@ def get_latest_bitcoin_price():
     return float(response_json['data'][0]['quote']['USD']['price']) # Convert the price to a floating point number
 
 def post_ifttt_webhook(event, value):
-    # logging.info('typeof value')
-    # print(value)
-    # currency_string = "{:,.2f}".format(value)
-    data = {'value1': value} # The payload that will be sent to IFTTT service
+
+    bitcoin_value = value
+    if isinstance(value, str) == False:
+        bitcoin_value = "{:,.2f}".format(value)
+
+    data = {'value1': bitcoin_value} # The payload that will be sent to IFTTT service
     logging.info(data)
     ifttt_event_url = IFTTT_WEBHOOK_URL.format(event)  # Inserts our desired event
     logging.info('Webhook sent for event, '+ event);
@@ -45,8 +47,9 @@ def format_bitcoin_history(bitcoin_history):
     for bitcoin_price in bitcoin_history:
         date = bitcoin_price['date'].strftime('%d.%m.%Y %H:%M') # Formats the date into a string: '24.02.2018 15:09'
         price = bitcoin_price['price']
+        currency_string = "{:,.2f}".format(price)
         # <b> (bold) tag creates bolded text
-        row = '{}: $<b>{}</b>'.format(date, price)  # 24.02.2018 15:09: $<b>10123.4</b>
+        row = '{}: $<b>{}</b>'.format(date, currency_string)  # 24.02.2018 15:09: $<b>10123.4</b>
         rows.append(row)
     # Use a <br> (break) tag to create a new line
     return '<br>'.join(rows)  # Join the rows delimited by <br> tag: row1<br>row2<br>row3
@@ -64,7 +67,7 @@ def main():
             post_ifttt_webhook("bitcoin_price_emergency", price)
 
         # Send a Telegram notification
-        if len(bitcoin_history) == 5: # Once we have 2 items in our bitcoin_history send an update
+        if len(bitcoin_history) == 2: # Once we have 2 items in our bitcoin_history send an update
             post_ifttt_webhook("bitcoin_price_update", format_bitcoin_history(bitcoin_history))
             # Reset the history
             bitcoin_history = []
